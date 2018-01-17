d74cbf0e1da4763475270084578df1d148567198
rufunt <jazzoo@mail.ru>
2018-01-17 20:33:32 +0300

    Add devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  #before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
+  #private
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :username
-  end
+  #def configure_permitted_parameters
+  #  devise_parameter_sanitizer.for(:sign_up) << :username
+  #end
 end
